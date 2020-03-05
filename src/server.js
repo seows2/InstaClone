e4c5e5c5295395da08fa0da2da7217660ce38270
서우석 <seows@seouseog-ui-MacBook-Pro.local>
2020-03-06 02:58:30 +0900

    [5]mailgun 연동

--- src/server.js
+++ src/server.js
@@ -1,10 +1,13 @@
 import dotenv from "dotenv";
+import path from "path";
 import { GraphQLServer } from "graphql-yoga";
 import logger from "morgan";
 import schema from "./schema";
+import { sendSecretMail } from "./utils";
 
-dotenv.config();
+dotenv.config({ path: path.resolve(__dirname, ".env") });
 
+//sendSecretMail("seows2@naver.com", "123");
 const PORT = process.env.PORT || 4000;
 
 const server = new GraphQLServer({ schema });
--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,37 @@
+import dotenv from "dotenv";
+import path from "path";
+dotenv.config({ path: path.resolve(__dirname, ".env") });
 import { adjectives, nouns } from "./word";
+import nodemailer from "nodemailer";
+import mg from "nodemailer-mailgun-transport";
 
 export const generateSecret = () => {
   const randomNumber = Math.floor(Math.random() * adjectives.length);
   return `${adjectives[randomNumber]} ${nouns[randomNumber]}`;
+};
+
+const auth = {
+  auth: {
+    api_key: process.env.MAILGUN_API_KEY,
+    domain: process.env.MAILGUN_DOMAIN
+  }
+};
+const nodemailerMailgun = nodemailer.createTransport(mg(auth));
+
+export const sendSecretMail = (adress, secret) => {
+  nodemailerMailgun.sendMail(
+    {
+      from: "Project@seows.com",
+      to: adress,
+      subject: `우석SNS 가입 인증 메일입니다.`,
+      html: `안녕하세요! 가입인증 메일입니다. <br>${secret}<br>로그인 하기 위해 위 단어를 앱/웹사이트에 복사해 붙여넣어주세요!`
+    },
+    err => {
+      if (err) {
+        console.log(`Error: ${err}`);
+      } else {
+        console.log(`전송 완료!`);
+      }
+    }
+  );
 };
